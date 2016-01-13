29a582b4e084bd894dd432322ae702943fceeb84
Rachel Lee <lee.rachel.jy@gmail.com>
2016-01-13 11:06:20 -0500

    refactor and change tests

--- binarySearch.js
+++ binarySearch.js
@@ -1,40 +1,23 @@
 var binarySearch = function(array, targetValue) {
-  
-    var min = 0;
-    var max = array.length - 1;
-    var guess;
-    var counter = 0;
-    
+
+    var length = array.length,
+        min = 0,
+        max = array.length - 1
+
     while(min <= max) {
-        
-        guess = Math.floor((max + min) / 2);
-        
-        counter++;
-        
-        if(array[guess] === targetValue) {
-            console.log('Took ' + counter + ' number of guesses')
-            return guess;
+        mid = Math.floor((max + min) / 2);
+
+        if(array[mid] === targetValue) {
+            return mid;
+        } else if(array[mid] < targetValue) {
+            min += 1;
+        } else {
+            max -= 1;
         }
-        
-        else if(array[guess] < targetValue) {
-            min = guess + 1;
-        }
-        
-        else {
-            max = guess - 1;
-        }
-        
     }
 
-  return -1;
+    return -1;
 
-};
-
-var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
-    41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
-
-var result = binarySearch(primes, 73);
-
-console.log("Found prime at index " + result);
+}
 
 module.exports = binarySearch;
--- spec/binarySearch.spec.js
+++ spec/binarySearch.spec.js
@@ -5,27 +5,27 @@ describe("A suite", function() {
   var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
     41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
-  it("takes two arguments", function() {
-    expect(binarySearchFunction.length).toEqual(2);
-  });
-
-  it("takes only a sorted array", function() {
-    
-  });
-
   it("returns -1 if the array does not contain target value", function() {
     expect(binarySearchFunction([1,2], 4)).toEqual(-1);
   });
 
-  it("returns index of the location in the array of three items", function() {
-    expect(binarySearchFunction([1,2,3],1)).toEqual(0);
+  it("returns index of the location in an array of three items", function() {
+    expect(binarySearchFunction([1,2,3], 3)).toEqual(2);
   });
 
-  it("returns index of the location in the array for larger arrays", function() {
+  it("returns index of the location in an array of four items", function() {
+    expect(binarySearchFunction([1,2,3,4], 3)).toEqual(2);
+  });
+
+  it("returns index of the location in an array of five items", function() {
+    expect(binarySearchFunction([1,2,3,4,5], 4)).toEqual(3);
+  });
+
+  it("returns index of the location in a large array", function() {
     expect(binarySearchFunction(primes, 73)).toEqual(20);
   });
 
-  it("returns -1 if the array does not contain target value for larger arrays", function() {
+  it("returns -1 if a large array does not contain target value", function() {
     expect(binarySearchFunction(primes, 4)).toEqual(-1);
   });
   
